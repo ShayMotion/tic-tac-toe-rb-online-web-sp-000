d89e02f6f68b008b3dddc1aea4aec534d073c04f
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-01 17:11:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -147,18 +147,6 @@ def play(board)
   input = gets
 end
 
-def play(board)
-  while !over?(board)
-    turn(board)
-  end
-  if won?(board)
-    puts "Congratulations " + winner(board) + "!"
-  elsif draw?(board)
-    puts "Cat's Game!"
-  else
-    #do nothing
-  end
-end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
